ea330a9ad581a9e0e94529a2bd2de13345bc974e
Marius Bora <marborav@gmail.com>
2020-03-07 00:23:00 +0100

    cobra and viper

---
+++ lib/protocol_command.go
@@ -0,0 +1,21 @@
+package lib
+
+type NcMiscSeedAck struct {}
+
+type NcUserClientVersionCheckReq struct {}
+
+type NcUserClientRightversionCheckAck struct {}
+
+type NcUserUsLoginReq struct {}
+
+type NcUserXtrapReq struct {}
+
+type NcUserXtrapAck struct {}
+
+type NcUserLoginAck struct {}
+
+type NcUserWorldStatusReq struct {}
+
+type NcUserWorldStatusAck struct {}
+
+
