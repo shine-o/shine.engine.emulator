4023ff0c9a901c30c05c6622742ec92a242b13d4
Marius Bora <marborav@gmail.com>
2020-03-10 22:38:41 +0100

    utils

--- utils.go
+++ utils.go
@@ -5,7 +5,7 @@ import (
 	"encoding/binary"
 )
 
-func readBinary(data []byte, nc interface{}) error {
+func ReadBinary(data []byte, nc interface{}) error {
 	buf := bytes.NewBuffer(data)
 	if err := binary.Read(buf, binary.LittleEndian, nc); err != nil {
 		log.Error(err)
@@ -14,15 +14,11 @@ func readBinary(data []byte, nc interface{}) error {
 	return nil
 }
 
-func writeBinary(nc interface{}) ([]byte, error) {
+func WriteBinary(nc interface{}) ([]byte, error) {
 	buf := new(bytes.Buffer)
 	if err := binary.Write(buf, binary.LittleEndian, nc); err != nil {
 		log.Fatal(err)
 		return nil, err
 	}
 	return buf.Bytes(), nil
-}
-
-func logOutboundPacket(pc *Command) { // should be used in only one place :(
-	log.Infof("Outbound packet %v", pc.Base.String())
 }
