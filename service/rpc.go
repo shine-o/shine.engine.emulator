23e0b48fdc07282c0cf4e30dfdad07de06abba0e
Marius Bora <marborav@gmail.com>
2020-03-12 23:57:15 +0100

    (feature) WorldServer connection info is read correctly by the client.

---
+++ service/rpc.go
@@ -0,0 +1,80 @@
+package service
+
+import (
+	"context"
+	"encoding/binary"
+	"encoding/hex"
+	networking "github.com/shine-o/shine.engine.networking"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	gs "shine.engine.game_structs"
+	lw "shine.engine.protocol-buffers/login-world"
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	lw.UnimplementedWorldServer
+}
+
+// There can be many Worlds, each World with its own Zones
+// for simplicity we use hardcoded one for now :)
+func (s *server) AvailableWorlds(ctx context.Context, in *lw.ClientMetadata) (*lw.WorldsInfo, error) {
+	select {
+	case <-ctx.Done():
+		return &lw.WorldsInfo{Info: []byte{0}}, status.Errorf(codes.Canceled, "context was canceled")
+	default:
+		nc := &gs.NcUserLoginAck{}
+		nc.NumOfWorld = byte(1)
+
+		var worlds [1]gs.WorldInfo
+		w1 := gs.WorldInfo{
+			WorldNumber: 0,
+			WorldName:   gs.Name4{},
+			WorldStatus: 1,
+		}
+		copy(w1.WorldName.Name[:], "INITIO")
+		copy(w1.WorldName.NameCode[:], []uint32{262, 16720, 17735, 76})
+		worlds[0] = w1
+
+		nc.NumOfWorld = byte(1)
+		nc.Worlds = worlds
+
+		if data, err := networking.WriteBinary(nc); err == nil {
+			return &lw.WorldsInfo{Info: data}, nil
+		} else {
+			return &lw.WorldsInfo{Info: []byte{0}}, err
+		}
+	}
+}
+
+func (s *server) ConnectionInfo(ctx context.Context, req *lw.SelectedWorld) (*lw.WorldConnectionInfo, error) {
+	select {
+	case <-ctx.Done():
+		return nil, status.Errorf(codes.Canceled, "context was canceled")
+	default:
+		//nc := gs.NcUserWorldSelectAck{
+		//	WorldStatus: 6,
+		//	Ip:          gs.Name4{},
+		//	Port:        9110,
+		//	ValidateNew: [32]uint16{},
+		//}
+
+		data := make([]byte, 0)
+		data = append(data, byte(6))
+		data = append(data, []byte("192.168.1.131")...)
+		data = append(data, []byte{0, 0, 0}...)
+
+		var port int16 = 9110
+		b := make([]byte, 2)
+		binary.LittleEndian.PutUint16(b, uint16(port))
+		data = append(data, b...)
+		log.Info(hex.EncodeToString(data))
+		return &lw.WorldConnectionInfo{Info: data}, nil
+
+		//if data, err := networking.WriteBinary(nc); err == nil {
+		//	return &lw.WorldConnectionInfo{Info: data}, nil
+		//} else {
+		//	return &lw.WorldConnectionInfo{Info: []byte{0}}, err
+		//}
+	}
+}
--- service/world.go
+++ service/world.go
@@ -3,20 +3,32 @@ package service
 import (
 	"context"
 	"encoding/hex"
+	"fmt"
 	"github.com/google/logger"
-	protocol "github.com/shine-o/shine.engine.protocol"
+	networking "github.com/shine-o/shine.engine.networking"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+	"io/ioutil"
+	"net"
 	"os"
 	"path/filepath"
+	lw "shine.engine.protocol-buffers/login-world"
+	"sync"
 )
 
-var log logger.Logger
+var log *logger.Logger
+
+func init() {
+	log = logger.Init("WorldLogger", true, true, ioutil.Discard)
+	log.Info("WorldLogger init()")
+}
 
 func Start(cmd *cobra.Command, args []string) {
 	ctx := context.Background()
-
-	s := &protocol.Settings{}
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	s := &networking.Settings{}
 
 	if xk, err := hex.DecodeString(viper.GetString("crypt.xorKey")); err != nil {
 		log.Error(err)
@@ -33,13 +45,77 @@ func Start(cmd *cobra.Command, args []string) {
 		s.CommandsFilePath = path
 	}
 
-	ch := make(map[uint16]func(ctx context.Context, pc *protocol.Command))
+	ch := make(map[uint16]func(ctx context.Context, pc *networking.Command))
 
 	// for now, nada
 
-	hw := protocol.NewHandlerWarden(ch)
+	hw := networking.NewHandlerWarden(ch)
 
-	ss := protocol.NewShineService(s, hw)
+	ss := networking.NewShineService(s, hw)
 
-	ss.Listen(ctx)
+	go selfRPC(ctx)
+
+	ss.Listen()
+}
+
+type InRPC struct {
+	services map[string]*grpc.ClientConn
+	mu       sync.Mutex
+}
+
+type OutRPC struct {
+	services map[string]*grpc.ClientConn
+	mu       sync.Mutex
+}
+
+// listen on gRPC TCP connections related to this project
+// not needed for now, as login is not expecting to act as server
+func selfRPC(ctx context.Context) {
+	select {
+	case <-ctx.Done():
+		return
+	default:
+		if viper.IsSet("gRPC.services.self") {
+			// snippet for loading yaml array
+			services := make([]map[string]string, 0)
+			var m map[string]string
+			servicesI := viper.Get("gRPC.services.self")
+			servicesS := servicesI.([]interface{})
+			for _, s := range servicesS {
+				serviceMap := s.(map[interface{}]interface{})
+				m = make(map[string]string)
+				for k, v := range serviceMap {
+					m[k.(string)] = v.(string)
+				}
+				services = append(services, m)
+			}
+			for _, v := range services {
+				go gRpcServers(ctx, v)
+			}
+		}
+	}
+}
+
+func gRpcServers(ctx context.Context, service map[string]string) {
+	select {
+	case <-ctx.Done():
+		return
+	default:
+		address := fmt.Sprintf(":%v", service["port"])
+		lis, err := net.Listen("tcp", address)
+		if err != nil {
+			log.Errorf("could listen on port %v for service %v : %v", service["port"], service["name"], err)
+		}
+		s := grpc.NewServer()
+		//switch service["name"] { in case more than one rpc service
+		//case "world":
+		//}
+		lw.RegisterWorldServer(s, &server{})
+
+		log.Infof("Loading gRPC server connections %v@::%v", service["name"], service["port"])
+
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}
 }
