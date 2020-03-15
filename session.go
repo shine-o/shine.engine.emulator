f9946aa282460d94f0901f2f4404bad25ffbf368
Marius Bora <marborav@gmail.com>
2020-03-16 00:43:31 +0100

    (cleaning) commented code

--- session.go
+++ session.go
@@ -1,20 +1,5 @@
 package networking
 
-//type Session struct {
-//	id string
-//	ServiceData interface {}
-//}
-//
-//func NewSession() * Session  {
-//	return &Session{
-//		id:	uuid.New().String(),
-//	}
-//}
-//
-//func (s * Session) Identifier() string {
-//	return s.id
-//}
-
 type SessionFactory interface {
 	New() Session
 }
