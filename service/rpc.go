aa97d6c86e69be24e5f6226f0d961b033be0dfb7
Marius Bora <marborav@gmail.com>
2020-04-10 01:56:30 +0200

    (refactor) rpc redo

--- service/rpc.go
+++ service/rpc.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	lw "github.com/shine-o/shine.engine.protocol-buffers/login-world"
 	"github.com/spf13/viper"
@@ -9,8 +10,6 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"net"
-	"os"
-	"sync"
 	"time"
 )
 
@@ -18,13 +17,9 @@ type server struct {
 	lw.UnimplementedLoginServer
 }
 
-// RPCClients that this service will use to communicate with other services
-type RPCClients struct {
-	services map[string]*grpc.ClientConn
-	mu       sync.Mutex
-}
-
 const gRPCTimeout = time.Second * 2
+
+var errBadRpcClient = errors.New("gRPC client is not present in the config file")
 
 func (s *server) AccountInfo(ctx context.Context, req *lw.User) (*lw.UserInfo, error) {
 
@@ -39,117 +34,40 @@ func (s *server) AccountInfo(ctx context.Context, req *lw.User) (*lw.UserInfo, e
 	}, nil
 }
 
-// dial gRPC services that are needed.
-func gRPCClients(ctx context.Context) {
-	select {
-	case <-ctx.Done():
-		return
-	default:
-		inRPC := &RPCClients{
-			services: make(map[string]*grpc.ClientConn),
-		}
+func newRpcConn(name string) (*grpc.ClientConn, error){
+	clientKey := fmt.Sprintf("gRPC.clients.%v", name)
+	if viper.IsSet(clientKey) {
+		host := viper.GetString(fmt.Sprintf("%v.%v", clientKey, "host"))
+		port := viper.GetString(fmt.Sprintf("%v.%v", clientKey, "port"))
+		address := fmt.Sprintf("%v:%v", host, port)
 
-		if viper.IsSet("gRPC.services.external") {
-			// snippet for loading yaml array
-			services := make([]map[string]string, 0)
-			var m map[string]string
-			servicesI := viper.Get("gRPC.services.external")
-			servicesS := servicesI.([]interface{})
-			for _, s := range servicesS {
-				serviceMap := s.(map[interface{}]interface{})
-				m = make(map[string]string)
-				for k, v := range serviceMap {
-					m[k.(string)] = v.(string)
-				}
-				services = append(services, m)
-			}
-
-			for _, v := range services {
-				address := fmt.Sprintf("%v:%v", v["host"], v["port"])
-				conn, err := grpc.Dial(address, grpc.WithInsecure())
-				if err != nil {
-					log.Errorf("could not connect service %v : %v", v["name"], err)
-					os.Exit(1)
-				}
-				log.Infof("[gRPC] client connection: %v@%v:%v", v["name"], v["host"], v["port"])
-				inRPC.services[v["name"]] = conn
-				go statusConn(ctx, v, conn)
-			}
-			grpcc = inRPC
+		conn, err := grpc.Dial(address, grpc.WithInsecure())
+		if err != nil {
+			log.Errorf("could not connect service %v : %v", name, err)
+			return &grpc.ClientConn{}, err
 		}
+		log.Infof("[gRPC] client connection: %v@%v:%v", name, host, port)
+		return conn, nil
 	}
+
+	return &grpc.ClientConn{}, errBadRpcClient
 }
 
-// listen on gRPC TCP connections related to this project
-// not needed for now, as login is not expecting to act as server
-func selfRPC(ctx context.Context) {
-	select {
-	case <-ctx.Done():
-		return
-	default:
-		if viper.IsSet("gRPC.services.self") {
-			// snippet for loading yaml array
-			services := make([]map[string]string, 0)
-			var m map[string]string
-			servicesI := viper.Get("gRPC.services.self")
-			servicesS := servicesI.([]interface{})
-			for _, s := range servicesS {
-				serviceMap := s.(map[interface{}]interface{})
-				m = make(map[string]string)
-				for k, v := range serviceMap {
-					m[k.(string)] = v.(string)
-				}
-				services = append(services, m)
-			}
-			for _, v := range services {
-				go gRPCServers(ctx, v)
-			}
-		}
-	}
-}
-
-func gRPCServers(ctx context.Context, service map[string]string) {
-	select {
-	case <-ctx.Done():
-		return
-	default:
-		address := fmt.Sprintf(":%v", service["port"])
+func newRpcServer(name string) {
+	clientKey := fmt.Sprintf("gRPC.servers.%v", name)
+	if viper.IsSet(clientKey) {
+		port := viper.GetString(fmt.Sprintf("%v.%v", clientKey, "port"))
+		address := fmt.Sprintf(":%v",  port)
 		lis, err := net.Listen("tcp", address)
 		if err != nil {
-			log.Errorf("could listen on port %v for service %v : %v", service["port"], service["name"], err)
+			log.Errorf("could listen on port %v for service %v : %v", name, port, err)
 		}
 		s := grpc.NewServer()
-
 		lw.RegisterLoginServer(s, &server{})
 
-		log.Infof("Loading gRPC server connections %v@::%v", service["name"], service["port"])
-
+		log.Infof("Loading gRPC server connection %v@::%v", name, port)
 		if err := s.Serve(lis); err != nil {
-			log.Fatalf("failed to serve: %v", err)
+			log.Errorf("failed to serve: %v", err)
 		}
 	}
-}
-
-func statusConn(ctx context.Context, service map[string]string, conn *grpc.ClientConn) {
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		default:
-			time.Sleep(120 * time.Second)
-			log.Infof("[gRPC] [client] [%v]: %v@%v:%v ", conn.GetState(), service["name"], service["host"], service["port"])
-		}
-	}
-}
-
-func cleanupRPC() {
-	grpcc.mu.Lock()
-	for _, s := range grpcc.services {
-		if err := s.Close(); err != nil {
-			log.Error(err)
-		} else {
-			log.Info("Closing down external gRPC connection")
-		}
-	}
-	grpcc.mu.Unlock()
 }
