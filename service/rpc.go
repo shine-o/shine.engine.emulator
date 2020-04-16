feb5b72fb2c876ac0247159bce3e086f133920bf
Marius Bora <marborav@gmail.com>
2020-04-16 22:53:06 +0200

    register zones

---
+++ service/rpc.go
@@ -0,0 +1,89 @@
+package service
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/shine-o/shine.engine.core/grpc/zone-master"
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+	"net"
+)
+
+type server struct {
+	zm.UnimplementedMasterServer
+}
+
+var errBadRPCClient = errors.New("gRPC client is not present in the config file")
+
+func (s *server) RegisterZone(ctx context.Context, zd *zm.ZoneDetails) (*zm.ZoneRegistered, error) {
+	// keep in redis:
+	// for each map, add new entry to registeredMaps with the map name as key and the zone connection info as value
+	var rm registeredMaps
+
+	res, err := redisClient.Get("zone-master").Result()
+	if err != nil {
+		rm = make(registeredMaps)
+	} else {
+		err = json.Unmarshal([]byte(res), &rm)
+	}
+
+	for _, m := range zd.Maps {
+		rm[m] = ZoneInfo{
+			IP:   zd.Conn.IP,
+			Port: zd.Conn.Port,
+		}
+		log.Infof("registering map %v for zone %v:%v", m, zd.Conn.IP, zd.Conn.Port)
+	}
+
+	return &zm.ZoneRegistered{
+		Success: true,
+		ZoneID:  "",
+		World: &zm.World{
+			IP:   viper.GetString("world.ip"),
+			Port: viper.GetInt32("world.port"),
+		},
+	}, nil
+}
+
+func (s *server) WhereIsMap(ctx context.Context, zd *zm.MapQuery) (*zm.ConnectionInfo, error) {
+	return &zm.ConnectionInfo{}, nil
+}
+
+func newRPCClient(name string) (*grpc.ClientConn, error) {
+	clientKey := fmt.Sprintf("gRPC.clients.%v", name)
+	if viper.IsSet(clientKey) {
+		host := viper.GetString(fmt.Sprintf("%v.%v", clientKey, "host"))
+		port := viper.GetString(fmt.Sprintf("%v.%v", clientKey, "port"))
+		address := fmt.Sprintf("%v:%v", host, port)
+
+		conn, err := grpc.Dial(address, grpc.WithInsecure())
+		if err != nil {
+			log.Errorf("could not connect service %v : %v", name, err)
+			return &grpc.ClientConn{}, err
+		}
+		log.Infof("[gRPC] client connection: %v@%v:%v", name, host, port)
+		return conn, nil
+	}
+	return &grpc.ClientConn{}, errBadRPCClient
+}
+
+func newRPCServer(name string) {
+	clientKey := fmt.Sprintf("gRPC.servers.%v", name)
+	if viper.IsSet(clientKey) {
+		port := viper.GetString(fmt.Sprintf("%v.%v", clientKey, "port"))
+		address := fmt.Sprintf(":%v", port)
+		lis, err := net.Listen("tcp", address)
+		if err != nil {
+			log.Errorf("could listen on port %v for service %v : %v", name, port, err)
+		}
+		s := grpc.NewServer()
+		zm.RegisterMasterServer(s, &server{})
+
+		log.Infof("Loading gRPC server connection %v@::%v", name, port)
+		if err := s.Serve(lis); err != nil {
+			log.Errorf("failed to serve: %v", err)
+		}
+	}
+}
---
+++ service/session.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/go-redis/redis/v7"
+	"github.com/spf13/viper"
+)
+
+var redisClient *redis.Client
+
+func initRedis() {
+	host := viper.GetString("session.redis.host")
+	port := viper.GetString("session.redis.port")
+	db := viper.GetInt("session.redis.db")
+	addr := fmt.Sprintf("%v:%v", host, port)
+	log.Infof("initializing redis instance: %v", addr)
+	client := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: "", // no password set
+		DB:       db, // use default DB
+	})
+	redisClient = client
+}
+
+func persist(ws *registeredMaps) error {
+	sd, err := json.Marshal(ws)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+	err = redisClient.Set("zone-master", sd, 0).Err()
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+	log.Infof("persisting maps %v ", string(sd))
+	return nil
+}
