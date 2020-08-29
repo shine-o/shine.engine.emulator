5cf492df310fa0fad8a581a4b4acf6680d97179e
Marius Bora <marborav@gmail.com>
2020-08-29 17:31:06 +0200

    small refactor

--- internal/app/world/character_workers.go
+++ internal/app/world/character_workers.go
@@ -125,65 +125,16 @@ func (w *world) characterSession() {
 					log.Errorf("expected event type %v but got %v", reflect.TypeOf(&updateShortcutsEvent{}).String(), reflect.TypeOf(ev).String())
 					return
 				}
-				c, err := character.Get(w.db, ev.characterID)
-				if err != nil {
-					log.Error(err)
-					return
-				}
-
-				storedShortcuts := structs.NcCharGetShortcutDataCmd{}
-
-				err = structs.Unpack(c.Options.Shortcuts, &storedShortcuts)
-				if err != nil {
-					log.Error(err)
-					return
-				}
-
-				var newShortcuts []structs.ShortCutData
-
-				for _, s1 := range ev.nc.Shortcuts {
-					exists := false
-					for j, s2 := range storedShortcuts.Shortcuts {
-						if s2.SlotNo == s1.SlotNo {
-							storedShortcuts.Shortcuts[j].CodeNo = s1.CodeNo
-							storedShortcuts.Shortcuts[j].Value = s1.Value
-							exists = true
-						}
-					}
-					if !exists {
-						newShortcuts = append(newShortcuts, s1)
-					}
-				}
-
-				storedShortcuts.Shortcuts = append(storedShortcuts.Shortcuts, newShortcuts...)
-
-				storedShortcuts.Count = uint16(len(storedShortcuts.Shortcuts))
-
-				data, err := structs.Pack(&storedShortcuts)
-
-				if err != nil {
-					log.Error(err)
-					return
-				}
-
-				c.Options.Shortcuts = data
-
-				err = character.Update(w.db, &c)
-				if err != nil {
-					log.Error(err)
-					return
-				}
-
-				nc := structs.NcCharOptionImproveShortcutDataAck{ErrCode: 8448}
-				ncCharOptionImproveSetShortcutDataAck(ev.np, &nc)
-
+				updateShortcutsLogic(w, ev)
 			}()
-		//case e := <-w.recv[updateGameSettings]:
-		//case e := <-w.recv[updateKeymap]:
+			//case e := <-w.recv[updateGameSettings]:
+			//case e := <-w.recv[updateKeymap]:
 
 		}
 	}
 }
+
+
 
 func handleCharacterLogin(ev *characterLoginEvent, w *world) {
 	s, ok := ev.np.Session.(*session)
